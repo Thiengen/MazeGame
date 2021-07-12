7f4d29c59ba4c4ab3e567e1b9c2acd2df3d5bd07
TG <thiengen987@gmail.com>
2021-07-12 17:26:42 +0800

    Tidy liao
    Tidy Codes

--- CamLabel.js
+++ CamLabel.js
@@ -1,5 +1,26 @@
 
 function OpenAndHideCamera() {
-    video = createCapture(VIDEO);
-    video.hide();
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function printLabel() {
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  fill(200);
+  text(label, width / 2, height - 16);
+}
+
+function classifierVideo(){
+  classifier.classify(video , gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
   }
+  label = results[0].label;
+  classifierVideo();
+}
+
