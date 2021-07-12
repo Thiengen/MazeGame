bf3a8e9fbcfa77dedc0f908b1adf4488ff93f140
Thiengen <67547600+Thiengen@users.noreply.github.com>
2021-07-12 17:51:38 +0800

    Teachable Machine (#2)
    Added teachable machine model for reading player's hand gesture to control movement.
    
    Co-authored-by: StevenChang21 <82698910+StevenChang21@users.noreply.github.com>

---
+++ CamLabel.js
@@ -0,0 +1,28 @@
+let label = " Waiting ..." ;
+let video ;
+let classifier ;
+function OpenAndHideCamera() {
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function printLabel() {
+  textSize(30);
+  textAlign(CENTER, CENTER);
+  fill(200);
+  text(label, width / 2, height - 16);
+}
+
+function classifierVideo(){
+  classifier.classify(video , gotResults);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  label = results[0].label;
+  classifierVideo();
+}
+
--- player.js
+++ player.js
@@ -9,20 +9,28 @@ function spawnPlayer(spawning_point){
 }
 
 function keyPressed() {
-  if (keyCode == 87 && !cell_player_is_in.walls[0]) {
+    if ((keyCode == 87 || label === "Up" )&& !cell_player_is_in.walls[0]) {
     move(directions.TOP);
+    print(label)
     return;
   }
-  else if (keyCode == 68 && !cell_player_is_in.walls[1]) {
+  else if ((keyCode == 68 || label === "Right" )&& !cell_player_is_in.walls[1]) {
     move(directions.RIGHT);
+    print(label)
     return;
   }
-  else if (keyCode == 83 && !cell_player_is_in.walls[2]) {
+  else if ((keyCode == 83 || label === "Down" )&& !cell_player_is_in.walls[2]) {
     move(directions.BOTTOM);
+    print(label)
     return;
   }
-  else if (keyCode == 65 && !cell_player_is_in.walls[3]) {
+  else if ((keyCode == 65 ||label === "Left" )&& !cell_player_is_in.walls[3]) {
     move(directions.LEFT);
+    print(label)
+    return;
+  }
+  else if (label == "Nothing "){
+    print(label)
     return;
   }
 }
