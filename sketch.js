9d0da6e4aee7b0ed1de819ad4294c623712c329a
StevenChang21 <82698910+StevenChang21@users.noreply.github.com>
2021-07-15 09:29:35 +0800

    Removed duplicated code
    Duplicate reference of ml5 library

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ function preload(){
 }
 
 function setup() {
-
   createCanvas(600, 600);
   maze = new Maze(30, width, height);
   maze.Generate();
@@ -21,5 +20,5 @@ function draw() {
   maze.Render(color(255,255,255), color(60, 60, 60));
   player.Render(color(47, 194, 86), color(10, 36, 17));
   printLabel();
-  PlayerMovementWithLabel();
+  //PlayerMovementWithLabel();
 }
